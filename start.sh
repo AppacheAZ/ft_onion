6930c4870dc4d037eaece11512afdc3241a2a460
MarcoAZ <marcoaz2001@outlook.com>
2023-05-31 01:29:34 +0200

    Añado juego

--- start.sh
+++ start.sh
@@ -3,3 +3,5 @@ docker rm pruebanginx
 docker build -t prueba .
 docker run -d -p 80:80 -p 4242:22 --name pruebanginx prueba
 docker exec -it pruebanginx /bin/bash -c "cat /var/lib/tor/hidden_service/hostname"
+
+
