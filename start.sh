3cc0961a937db1c90e53ad23ee15eb6b820a665d
Marco Alvarez Zambrano <marcoalv@c3r5s3.42urduliz.com>
2023-06-05 21:30:18 +0200

    Solucionado el problema del restart añadiendolo al final del script que inicia el contenedor

--- start.sh
+++ start.sh
@@ -5,5 +5,6 @@ docker build -t prueba .
 docker run -d -p 80:80 -p 4242:4242 --name pruebanginx prueba
 echo "The Onion URL of the page is:"
 docker exec -it pruebanginx /bin/bash -c "cat /var/lib/tor/hidden_service/hostname"
+docker exec -it pruebanginx /bin/bash -c "service ssh restart"
 
 
